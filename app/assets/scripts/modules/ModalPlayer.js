2d7eeec9b7028f3fe03aaedb51f575496ea2751e
hilmerx <joel.hilme@gmail.com>
2017-12-13 14:13:47 +0100

    fixed pause on click

--- app/assets/scripts/modules/ModalPlayer.js
+++ app/assets/scripts/modules/ModalPlayer.js
@@ -58,70 +58,70 @@ class ModalPlayer {
         dataType: 'json',
         url:"assets/json/projects/"+moduleName+'.json',
         })
-            .done( (data) => {
+        .done( (data) => {
 
-                $(".modal__title").html(data.title)
-                $(".modal__director").html(data.directorText)
-
-
-
-                data.movies.forEach(function(val, num) {
-
-                    if(data.movies.length === 2){
-                        $(".plyr__container").append('<video poster="" controls crossorigin class="modal__video-player"><source src="" type="video/mp4" class="modal__video-source"></video>')
-                    } else {
-                        $(".plyr__container").append('<video poster="" controls crossorigin class="modal__video-player"><source src="" type="video/mp4" class="modal__video-source"></video>')
-                    }
+            $(".modal__title").html(data.title)
+            $(".modal__director").html(data.directorText)
 
 
-                    player[num] = $(".modal__video-player")[num]
-                    source[num] = $(".modal__video-source")[num]
-                })
 
-                var instances = plyr.setup({
-                    // autoplay: true,
-                    // debug: true
-                });
+            data.movies.forEach(function(val, num) {
 
-
-                source.forEach(function(instance, num) {
-                    instance.setAttribute('src', data.movies[num])
-                });
-
-                if (player.length===1){
-                    $('.plyr').css("display", "block").css("max-width", "100%")
-                }else if (player.length===2){
-                    $('.plyr').css("display", "inline-block").css("max-width", "50%")
+                if(data.movies.length === 2){
+                    $(".plyr__container").append('<video poster="" controls crossorigin class="modal__video-player"><source src="" type="video/mp4" class="modal__video-source"></video>')
                 } else {
-                    $('.plyr').css("display", "inline-block").css("max-width", "50%")
+                    $(".plyr__container").append('<video poster="" controls crossorigin class="modal__video-player"><source src="" type="video/mp4" class="modal__video-source"></video>')
                 }
 
 
-                player.forEach(function(instance) {
-                    instance.load();
+                player[num] = $(".modal__video-player")[num]
+                source[num] = $(".modal__video-source")[num]
+            })
+
+            var instances = plyr.setup({
+                // autoplay: true,
+                // debug: true
+            });
+
+
+            source.forEach(function(instance, num) {
+                instance.setAttribute('src', data.movies[num])
+            });
+
+            if (player.length===1){
+                $('.plyr').css("display", "block").css("max-width", "100%")
+            }else if (player.length===2){
+                $('.plyr').css("display", "inline-block").css("max-width", "50%")
+            } else {
+                $('.plyr').css("display", "inline-block").css("max-width", "50%")
+            }
+
+
+            player.forEach(function(instance) {
+                instance.load();
+            })
+
+
+            $(".modal__descripitive-text").html(data.text)
+        })
+        .done(() => {            
+            let instances = plyr.get();
+
+            let divs = $(".modal__video-player")
+
+            for (let x =  0; x < instances.length; x++) {
+                const instance = instances[x]
+                const activeDiv = divs[x]
+                activeDiv.addEventListener('click', () => {
+                    if (!instance.isPaused()) {
+                        setTimeout( () => {
+                            instance.pause()
+                        }, 50)
+                    }
                 })
+            }
 
-
-                $(".modal__descripitive-text").html(data.text)
-            })
-            .done(() => {            
-                let instances = plyr.get();
-
-                let divs = $(".modal__video-player")
-
-                for (let x =  0; x < instances.length; x++) {
-                    const instance = instances[x]
-                    const activeDiv = divs[x]
-                    activeDiv.addEventListener('click', () => {
-                        if (!instance.isPaused()) {
-                            setTimeout( () => {
-                                instance.pause()
-                            }, 50)
-                        }
-                    })
-                }
-
-            })
+        })
     }
 
     closeModal() {
